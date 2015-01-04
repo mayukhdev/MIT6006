c7b541098ddb0a6ddc8b98afa6158977aabd8e7a
Mayukh <mayukhdev@gmail.com>
2015-01-04 16:15:58 +0530

    modified dp_fib to one function.

--- DP_fib.py
+++ DP_fib.py
@@ -1,12 +1,13 @@
-memo = {}
 def DP_fib(n):
-    if n in memo:
-        return memo[n]
-    if n==0:
-        f=0
-    elif n<=2:
-        f=1
-    else:
-        f = DP_fib(n-1) + DP_fib(n-2)
-    memo[n] = f
-    return f
+    memo = {}
+    def fib(n):
+        if n in memo:  
+            return memo[n]
+        if n<=2:
+            f=1
+        else:
+            f = fib(n-1) + fib(n-2)
+        memo[n] = f
+        print "memo[" + str(n)+ "]=" + str(f)
+        return f
+    print fib(n)
